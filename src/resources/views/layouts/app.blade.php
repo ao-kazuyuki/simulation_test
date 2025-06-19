99e58a426d6e5025694b341539d843d6f24ef5a6
root <root@fs010001.>
2025-06-19 12:34:14 +0900

    header.css追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>coachtechフリマ</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     @yield('css')
 </head>
@@ -14,7 +15,7 @@
     <header class="header">
         <div class="header-item__group">
             <div class="header-logo__area">
-                <img class="header-logo" src="{{ asset('img/logo.svg') }}">
+                <img src="{{ asset('img/logo.svg') }}">
             </div>
             @yield('header-menu')
         </div>
